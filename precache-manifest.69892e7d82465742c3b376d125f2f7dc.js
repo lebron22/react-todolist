633bb943ff0a6c8501d5780fdaf49940134d31ed
Kamil Bronisz <kbronisz8@gmail.com>
2019-11-22 13:35:24 +0100

    Updates

---
+++ precache-manifest.69892e7d82465742c3b376d125f2f7dc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337977e8360c68849a4eb1285ceed49c",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "2a0989b7eaf884a13c93",
+    "url": "/react-todolist/static/js/2.bf52dbbd.chunk.js"
+  },
+  {
+    "revision": "71a8137d0ea7352078e4",
+    "url": "/react-todolist/static/js/main.c8d80f4c.chunk.js"
+  },
+  {
+    "revision": "dd2a0c730e02a59d4852",
+    "url": "/react-todolist/static/js/runtime~main.8b2e420c.js"
+  }
+]);
